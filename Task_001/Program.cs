aea7163eae73061ca1967a012c12ff7c4b1989b2
Vlad Aver <averyanau19gmail.com>
2023-12-02 16:08:53 +0100

    Добавил решение второй задачи

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -3,54 +3,48 @@
 
 using System;
 
-class Program
+while (true)
 {
-    static void Main()
+    Console.Write("Введите целое число (или 'q' для выхода): ");
+    string input = Console.WriteLine(Console.ReadLine());;
+
+    // Проверяем, если введен 'q', завершаем программу
+    if (input.ToLower() == "q")
     {
-        while (true)
-        {
-            Console.Write("Введите целое число (или 'q' для выхода): ");
-            string input = Console.ReadLine();
-
-            // Проверяем, если введен 'q', завершаем программу
-            if (input.ToLower() == "q")
-            {
-                Console.WriteLine("Программа завершена.");
-                break;
-            }
-
-            // Пытаемся преобразовать введенную строку в целое число
-            if (int.TryParse(input, out int number))
-            {
-                // Проверяем, если сумма цифр числа четная
-                if (IsSumEven(number))
-                {
-                    Console.WriteLine($"Сумма цифр числа {number} четная. Программа завершена.");
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine($"Сумма цифр числа {number} нечетная.");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число или 'q' для выхода.");
-            }
-        }
+        Console.WriteLine("Программа завершена.");
+        break;
     }
 
-    static bool IsSumEven(int num)
+    // Пытаемся преобразовать введенную строку в целое число
+    if (int.TryParse(input, out int number))
     {
-        int sum = 0;
-        // Вычисляем сумму цифр числа
-        while (num != 0)
+        // Проверяем, если сумма цифр числа четная
+        if (IsSumEven(number))
         {
-            sum += num % 10;
-            num /= 10;
+            Console.WriteLine($"Сумма цифр числа {number} четная. Программа завершена.");
+            break;
         }
-        // Проверяем, четная ли сумма
-        return sum % 2 == 0;
+        else
+        {
+            Console.WriteLine($"Сумма цифр числа {number} нечетная.");
+        }
     }
+    else
+    {
+        Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число или 'q' для выхода.");
+    }
+}
+
+bool IsSumEven(int num)
+{
+    int sum = 0;
+    // Вычисляем сумму цифр числа
+    while (num != 0)
+    {
+        sum += num % 10;
+        num /= 10;
+    }
+    // Проверяем, четная ли сумма
+    return sum % 2 == 0;
 }
 
---
+++ Task_002/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 2. Задайте массив заполненный случайными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Задаем размер массива
+        int arraySize = 10;
+        
+        // Создаем массив и заполняем случайными трехзначными числами
+        int[] array = new int[arraySize];
+        Random random = new Random();
+        for (int i = 0; i < arraySize; i++)
+        {
+            array[i] = random.Next(100, 1000);
+        }
+
+        // Выводим массив на экран
+        Console.WriteLine("Сгенерированный массив:");
+        PrintArray(array);
+
+        // Находим и выводим количество четных чисел в массиве
+        int evenCount = CountEvenNumbers(array);
+        Console.WriteLine($"Количество четных чисел в массиве: {evenCount}");
+    }
+
+    static void PrintArray(int[] arr)
+    {
+        foreach (int num in arr)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine();
+    }
+
+    static int CountEvenNumbers(int[] arr)
+    {
+        int count = 0;
+        foreach (int num in arr)
+        {
+            if (num % 2 == 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
